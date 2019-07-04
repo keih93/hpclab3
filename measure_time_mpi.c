bdbc5f678358a03dc259917c5ce1fe316574d7da
keih93 <nnquang7114@gmail.com>
2019-07-04 19:31:21 +0200

    Update measure_time_mpi.c

--- measure_time_mpi.c
+++ measure_time_mpi.c
@@ -13,7 +13,7 @@
 
 #define START_TIMEMEASUREMENT(name)   struct timeval __FILE__##__func__##name##actualtime; \
 gettimeofday(&__FILE__##__func__##name##actualtime, NULL); \
-double __FILE__##__func__##name##s_time = (double)__FILE__##__func__##name##actualtime.tv_sec+((double)__FILE__##__func__##name##actualtime.tv_usec/1000000.0) 
+double __FILE__##__func__##name##s_time = (double)__FILE__##__func__##name##actualtime.tv_sec+((double)__FILE__##__func__##name##actualtime.tv_usec/1000000.0)
 
 
 #define END_TIMEMEASUREMENT(name, res) gettimeofday(&__FILE__##__func__##name##actualtime, NULL); \
@@ -21,16 +21,20 @@ res = (double)__FILE__##__func__##name##actualtime.tv_sec+((double)__FILE__##__f
 
 
 int main (int c, char **v) {
-  
+
+  int rank, size, i;
+  MPI_Status status;
+
   MPI_Init(&c, &v);
-  
- 
+
   // get rank and number of processes and print it out
-                                                                                                 
-                                                                                                 
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  printf("size: %d \n",size );
+  printf("rank: %d \n",rank );
   // run task funktion and measure duration and calculate average and max times
 
-  
+
   MPI_Finalize();
-  
+  return 0;
 }
