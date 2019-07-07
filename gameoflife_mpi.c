513e830597ab81ab16db2287e744653b7cf7f1a6
keih93 <nnquang7114@gmail.com>
2019-07-07 17:08:23 +0200

    Revert "Update gameoflife_mpi.c"
    This reverts commit 4790b4b7ccf8549db24d8c25d6d438c265180282.

--- gameoflife_mpi.c
+++ gameoflife_mpi.c
@@ -382,14 +382,14 @@ void apply_periodic_boundaries(char *field, int width, int height) {
             MPI_Isend(sendcellstb[i], width + 1, MPI_CHAR, neighborank[i], 1, cart_comm, &(request[numrequest1]));
             MPI_Irecv(recvcellstb[i], width + 1, MPI_CHAR, neighborank[i], 1, cart_comm, &(request[numrequest1 + 1]));
             numrequest1 = numrequest1 + 2;
-            printf("%d h %d numrequest %d recvcells %c sendcells %c \n", rank_cart, i, numrequest1, recvcellstb[i][width],sendcellstb[i][width]);
+            //printf("%d h %d numrequest %d recvcells %c sendcells %c \n", rank_cart, i, numrequest1, recvcellstb[i][width],sendcellstb[i][width]);
           }else{
             //printf("%d neighbor %d\n",rank_cart, neighborank[i]);
             //printf("%d h %d numrequest %d recvcells %c sendcells %c \n", rank_cart, i, numrequest1, recvcellslr[i-2][height],sendcellslr[i-2][height]);
             MPI_Isend(sendcellslr[i-2], height + 1, MPI_CHAR, neighborank[i], 1, cart_comm, &(request[numrequest1]));
             MPI_Irecv(recvcellslr[i-2], height + 1, MPI_CHAR, neighborank[i], 1, cart_comm, &(request[numrequest1 + 1]));
             numrequest1 = numrequest1 + 2;
-            printf("%d h %d numrequest %d recvcells %c sendcells %c \n", rank_cart, i, numrequest1, recvcellslr[i-2][height],sendcellslr[i-2][height]);
+            //printf("%d h %d numrequest %d recvcells %c \n", rank_cart, i, numrequest1, recvcellslr[i-2][height]);
           }
         }
     }
