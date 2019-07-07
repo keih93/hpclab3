97a69224ce6360ca44ecd8f217923286fc92da55
keih93 <nnquang7114@gmail.com>
2019-07-07 14:47:25 +0200

    Update gameoflife_mpi.c

--- gameoflife_mpi.c
+++ gameoflife_mpi.c
@@ -388,7 +388,7 @@ void apply_periodic_boundaries(char *field, int width, int height) {
             MPI_Isend(&sendcellslr[i-2], height + 1, MPI_CHAR, neighborank[i], 1, cart_comm, &(request[numrequest1]));
             MPI_Irecv(&recvcellslr[i-2], height + 1, MPI_CHAR, neighborank[i], 1, cart_comm, &(request[numrequest1 + 1]));
             numrequest1 = numrequest1 + 2;
-            printf("%d h %d numrequest %d recvcells %c sendcells %c \n", rank_cart, i, numrequest1, recvcellslr[i][height],sendcellslr[i][width]);
+            printf("%d h %d numrequest %d recvcells %c sendcells %c \n", rank_cart, i, numrequest1, recvcellslr[i][height],sendcellslr[i][height]);
           }
         }
     }
