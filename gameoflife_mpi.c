303d972abbaad007860facde9b46a2975db36cd9
keih93 <nnquang7114@gmail.com>
2019-07-07 17:10:07 +0200

    Revert "Update gameoflife_mpi.c"
    This reverts commit 90f72feb7ed7aa5efd8f604b4a0c0292c65b7cdd.

--- gameoflife_mpi.c
+++ gameoflife_mpi.c
@@ -400,7 +400,7 @@ void apply_periodic_boundaries(char *field, int width, int height) {
     //printf("%d after send and recved \n", rank_cart);
 
     for (int z = 0; z < 2; z++) {
-        printf("%d checking cells copy %c \n",rank_cart,recvcellstb[z][width] );
+        printf("%d checking cells copy %c \n",rank_cart,recvcellstb[k][width] );
         if (recvcellstb[z][width] == 'b') {
             for (int x = 0; x < width - 1; x++) {
                 int l = calcIndex(width, x, height - 1);
