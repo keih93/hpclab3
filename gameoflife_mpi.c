dbac1b488e80b9199f1ac8d4dd31a868535085fa
keih93 <nnquang7114@gmail.com>
2019-07-07 15:28:36 +0200

    Update gameoflife_mpi.c

--- gameoflife_mpi.c
+++ gameoflife_mpi.c
@@ -382,14 +382,14 @@ void apply_periodic_boundaries(char *field, int width, int height) {
             MPI_Isend(sendcellstb[i], width + 1, MPI_CHAR, neighborank[i], 1, cart_comm, &(request[numrequest1]));
             MPI_Irecv(recvcellstb[i], width + 1, MPI_CHAR, neighborank[i], 1, cart_comm, &(request[numrequest1 + 1]));
             numrequest1 = numrequest1 + 2;
-            printf("%d h %d numrequest %d recvcells %c sendcells %c \n", rank_cart, i, numrequest1, recvcellstb[i][width],sendcellstb[i][width]);
+            //printf("%d h %d numrequest %d recvcells %c sendcells %c \n", rank_cart, i, numrequest1, recvcellstb[i][width],sendcellstb[i][width]);
           }else{
             //printf("%d neighbor %d\n",rank_cart, neighborank[i]);
             //printf("%d h %d numrequest %d recvcells %c sendcells %c \n", rank_cart, i, numrequest1, recvcellslr[i-2][height],sendcellslr[i-2][height]);
             MPI_Isend(sendcellslr[i-2], height + 1, MPI_CHAR, neighborank[i], 1, cart_comm, &(request[numrequest1]));
             MPI_Irecv(recvcellslr[i-2], height + 1, MPI_CHAR, neighborank[i], 1, cart_comm, &(request[numrequest1 + 1]));
             numrequest1 = numrequest1 + 2;
-            printf("%d h %d numrequest %d recvcells %c sendcells %c \n", rank_cart, i, numrequest1, recvcellslr[i-2][height],sendcellslr[i-2][height]);
+            //printf("%d h %d numrequest %d recvcells %c sendcells %c \n", rank_cart, i, numrequest1, recvcellslr[i-2][height],sendcellslr[i-2][height]);
           }
         }
     }
@@ -397,10 +397,12 @@ void apply_periodic_boundaries(char *field, int width, int height) {
         //printf("%d before MPI_Waitall countneighbor %d request %d\n", rank_cart, countneighbor,(sizeof(request) / sizeof(MPI_Request)));
         MPI_Waitall(countside, request, status);
     }
+    for (int i = 0; i < 4; i++) {
+        printf("%d h %d numrequest %d recvcells %c sendcells %c \n", rank_cart, i, numrequest1, recvcellslr[i-2][height],sendcellslr[i-2][height]);
+    }
     //printf("%d after send and recved \n", rank_cart);
 
     for (int z = 0; z < 2; z++) {
-      printf("%d checking cells copy %c z %d \n",rank_cart,recvcellslr[z][width],z );
         if (recvcellstb[z][width] == 'b') {
           printf("%d checking cells copy %c z %d \n",rank_cart,recvcellstb[z][width],z );
             for (int x = 0; x < width - 1; x++) {
